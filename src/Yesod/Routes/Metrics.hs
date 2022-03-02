6aa8625a394c33fb384a524bedab186f9a5dca09
Mrinal Purohit <github@mrinalpurohit.in>
2022-03-02 17:10:58 +0530

    Remove logging of latency

--- src/Yesod/Routes/Metrics.hs
+++ src/Yesod/Routes/Metrics.hs
@@ -228,7 +228,6 @@ metricsWithResourceTrees ymc resources yesodMetrics app req respond = do
           updateTotalRequests
           
           let latency = (round $ 1000 * (diffUTCTime after before))
-          print $ "Latency : " ++ (show latency)
           updateMaxLatency ((alterRouteName ymc) routeName) latency
           updateMinLatency ((alterRouteName ymc) routeName) latency
           updateAvgAndPercentiles ((alterRouteName ymc) routeName) latency
